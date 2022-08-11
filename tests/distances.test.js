f956756338e8cbac1cdc62febdab133a66a4fbd3
Sean McCarthy <sean+github@clanmccarthy.net>
2022-08-11 21:12:54 +1000

    Updating distances test

--- tests/distances.test.js
+++ tests/distances.test.js
@@ -1,11 +1,12 @@
 const Grid = require('../modules/grid.js');
 const Distances = require('../modules/distances.js');
 
-test('#on', () => {
+test('#calculateFrom', () => {
     const grid = new Grid(3, 3);
     grid.at(0, 0).link(grid.at(0, 1));
     grid.at(0, 1).link(grid.at(0, 2));
-    Distances.on(grid, 0, 0);
+    const dist = new Distances(grid);
+    dist.calculateFrom(grid.at(0, 0));
     const cell = grid.at(0, 2);
     expect(cell.distance).toBe(2);
 });
